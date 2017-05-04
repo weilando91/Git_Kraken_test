68cbe3d095e9568c774cbcbe97d6d193bf6a9d23
weilando91 <wehland91@hotmail.com>
2017-05-04 13:39:48 +0200

    Test commit from branch

--- git/main.c
+++ git/main.c
@@ -1,7 +1,107 @@
-/*
- * main.c
- */
-int main(void) {
-	
-	return 0;
+/*****************************************************************************
+* University of Southern Denmark
+* Embedded Programming (EMP)
+*
+* MODULENAME.: main.c
+*
+* PROJECT....: EMP
+*
+* DESCRIPTION: Assignment 2, main module. No main.h file.
+*
+* Change Log:
+*****************************************************************************
+* Date    Id    Change
+* YYMMDD
+* --------------------
+* 150215  MoH   Module created.
+*
+*****************************************************************************/
+
+/***************************** Include files *******************************/
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+#include "emp_type.h"
+#include "systick_frt.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "status_led.h"
+#include "leds.h"
+#include "adc.h"
+
+/*****************************    Defines    *******************************/
+#define USERTASK_STACK_SIZE configMINIMAL_STACK_SIZE
+#define IDLE_PRIO 0
+#define LOW_PRIO  1
+#define MED_PRIO  2
+#define HIGH_PRIO 3
+
+
+/*****************************   Constants   *******************************/
+
+/*****************************   Variables   *******************************/
+
+/*****************************   Functions   *******************************/
+
+
+int putChar()
+/*****************************************************************************
+*   Input    :  -
+*   Output   :  Result
+*   Function :  putChar for FreeRTOS debug functionality.
+*****************************************************************************/
+{
+  return(0);
 }
+
+static void setupHardware(void)
+/*****************************************************************************
+*   Input    :  -
+*   Output   :  -
+*   Function :
+*****************************************************************************/
+{
+  // TODO: Put hardware configuration and initialisation in here
+
+  // Warning: If you do not initialize the hardware clock, the timings will be inaccurate
+  init_systick();
+  status_led_init();
+  init_adc();
+}
+
+
+
+int main(void)
+/*****************************************************************************
+*   Input    :
+*   Output   :
+*   Function : The super loop.
+******************************************************************************/
+{
+  portBASE_TYPE return_value = pdTRUE;
+
+  setupHardware();
+
+  // Start the tasks.
+  // ----------------
+  return_value &= xTaskCreate( status_led_task, ( signed portCHAR * ) "Status_led", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
+  return_value &= xTaskCreate( red_led_task,    ( signed portCHAR * ) "Red_led",    USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
+  return_value &= xTaskCreate( yellow_led_task, ( signed portCHAR * ) "Yellow_led", USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
+  return_value &= xTaskCreate( green_led_task,  ( signed portCHAR * ) "green_led",  USERTASK_STACK_SIZE, NULL, LOW_PRIO, NULL );
+
+  if (return_value != pdTRUE)
+  {
+    GPIO_PORTD_DATA_R &= 0xBF;  // Turn on status LED.
+    while(1);  // cold not create one or more tasks.
+  }
+
+
+  // Start the scheduler.
+  // --------------------
+  vTaskStartScheduler();
+
+  // Will only get here, if there was insufficient memory.
+  // -----------------------------------------------------
+  return 1;
+}
+
+/****************************** End Of Module *******************************/
--- git/startup_frt.c
+++ git/startup_frt.c
@@ -1,6 +1,6 @@
 //*****************************************************************************
 //
-// Startup code for use with TI's Code Composer Studio.
+// Startup code for use with TI's Code Composer Studio and GNU tools.
 //
 // Copyright (c) 2011-2014 Texas Instruments Incorporated.  All rights reserved.
 // Software License Agreement
@@ -36,18 +36,32 @@ static void IntDefaultHandler(void);
 
 //*****************************************************************************
 //
-// External declaration for the reset handler that is to be called when the
-// processor is started
+// Forward declaration of FreeRTOS handlers.
 //
 //*****************************************************************************
-extern void _c_int00(void);
+void vPortSVCHandler( void ) __attribute__ (( naked ));
+void xPortPendSVHandler( void ) __attribute__ (( naked ));
+void xPortSysTickHandler( void );
+
+
+
+#ifndef HWREG
+#define HWREG(x) (*((volatile uint32_t *)(x)))
+#endif
 
 //*****************************************************************************
 //
-// Linker variable that marks the top of the stack.
+// The entry point for the application.
 //
 //*****************************************************************************
-extern uint32_t __STACK_TOP;
+extern int main(void);
+
+//*****************************************************************************
+//
+// Reserve space for the system stack.
+//
+//*****************************************************************************
+static uint32_t pui32Stack[128];
 
 //*****************************************************************************
 //
@@ -63,10 +77,10 @@ extern uint32_t __STACK_TOP;
 // the program if located at a start address other than 0.
 //
 //*****************************************************************************
-#pragma DATA_SECTION(g_pfnVectors, ".intvecs")
+__attribute__ ((section(".intvecs")))
 void (* const g_pfnVectors[])(void) =
 {
-    (void (*)(void))((uint32_t)&__STACK_TOP),
+    (void (*)(void))((uint32_t)pui32Stack + sizeof(pui32Stack)),
                                             // The initial stack pointer
     ResetISR,                               // The reset handler
     NmiSR,                                  // The NMI handler
@@ -78,11 +92,11 @@ void (* const g_pfnVectors[])(void) =
     0,                                      // Reserved
     0,                                      // Reserved
     0,                                      // Reserved
-    IntDefaultHandler,                      // SVCall handler
+    vPortSVCHandler,                        // FreeRTOS SVCall Handler
     IntDefaultHandler,                      // Debug monitor handler
     0,                                      // Reserved
-    IntDefaultHandler,                      // The PendSV handler
-    IntDefaultHandler,                      // The SysTick handler
+    xPortPendSVHandler,                     // FreeRTOS PendSV Handler
+    xPortSysTickHandler,                    // FreeRTOS SysTick Handler
     IntDefaultHandler,                      // GPIO Port A
     IntDefaultHandler,                      // GPIO Port B
     IntDefaultHandler,                      // GPIO Port C
@@ -226,6 +240,19 @@ void (* const g_pfnVectors[])(void) =
 
 //*****************************************************************************
 //
+// The following are constructs created by the linker, indicating where the
+// the "data" and "bss" segments reside in memory.  The initializers for the
+// for the "data" segment resides immediately following the "text" segment.
+//
+//*****************************************************************************
+extern uint32_t __data_load__;
+extern uint32_t __data_start__;
+extern uint32_t __data_end__;
+extern uint32_t __bss_start__;
+extern uint32_t __bss_end__;
+
+//*****************************************************************************
+//
 // This is the code that gets called when the processor first starts execution
 // following a reset event.  Only the absolutely necessary set is performed,
 // after which the application supplied entry() routine is called.  Any fancy
@@ -237,12 +264,46 @@ void (* const g_pfnVectors[])(void) =
 void
 ResetISR(void)
 {
+    uint32_t *pui32Src, *pui32Dest;
+
     //
-    // Jump to the CCS C initialization routine.  This will enable the
-    // floating-point unit as well, so that does not need to be done here.
+    // Copy the data segment initializers from flash to SRAM.
     //
-    __asm("    .global _c_int00\n"
-          "    b.w     _c_int00");
+    pui32Src = &__data_load__;
+    for(pui32Dest = &__data_start__; pui32Dest < &__data_end__; )
+    {
+        *pui32Dest++ = *pui32Src++;
+    }
+
+    //
+    // Zero fill the bss segment.
+    //
+    __asm("    ldr     r0, =__bss_start__\n"
+          "    ldr     r1, =__bss_end__\n"
+          "    mov     r2, #0\n"
+          "    .thumb_func\n"
+          "zero_loop:\n"
+          "        cmp     r0, r1\n"
+          "        it      lt\n"
+          "        strlt   r2, [r0], #4\n"
+          "        blt     zero_loop");
+
+    //
+    // Enable the floating-point unit.  This must be done here to handle the
+    // case where main() uses floating-point and the function prologue saves
+    // floating-point registers (which will fault if floating-point is not
+    // enabled).  Any configuration of the floating-point unit using DriverLib
+    // APIs must be done here prior to the floating-point unit being enabled.
+    //
+    // Note that this does not use DriverLib since it might not be included in
+    // this project.
+    //
+    HWREG(0xE000ED88) = ((HWREG(0xE000ED88) & ~0x00F00000) | 0x00F00000);
+    
+    //
+    // Call the application's entry point.
+    //
+    main();
 }
 
 //*****************************************************************************
